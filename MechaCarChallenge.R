8874c0e84bbceddf41409be11e2a2ea29cc15116
Taravat <eau0715362@gmail.com>
2021-07-25 15:44:29 -0700

    Del 2 added.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,10 +4,21 @@
 library(dplyr)
 
 # Read in dataset
-mechacar_table <- read.csv("MechaCar_mpg.csv",check.names = F,stringsAsFactors = F)
+mechaCar <- read.csv("MechaCar_mpg.csv",check.names = F,stringsAsFactors = F)
 
 # Perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar)
 
 # Summarize linear model
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_table))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechaCar))
+
+# Deliverable 2
+
+# Read in dataset
+suspension_coil <- read.csv("Suspension_Coil.csv",check.names = F,stringsAsFactors = F)
+
+# Retrieve the mean, median, variance, and std of the suspension coil's PSI column
+total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+# Group each manufacturing lot by the mean, median, variance and std
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
